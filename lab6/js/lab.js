982a95a3552f2cca7ac0f2ed24394e43f9d12ac8
DiegoG0121 <dgarciamain20.us@gmail.com>
2024-10-24 17:52:13 -0700

    Changes to JS

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -19,3 +19,45 @@ function main() {
 
 // let's get this party started
 main();
+
+// list of modes of transportation
+myMainTransportation = ["Bus", "Scooter", "Car", "Bike"];
+
+// this contains a list of information towards my main form of transportation
+const wipInfo = {
+  make: "NineBot-SegWay",
+  model: "G30Max Electric Kick", 
+  color: "black",
+  year: 2019,
+  age: function(){ 
+    return 2024 - this.year;
+  }
+};
+
+// adds spaces between each item in the list when printed out into text format
+function addSpace(listInput){
+
+  this.listInput = listInput;
+  newList = [];
+  for(let i = 0; i < this.listInput.length; i++){
+    newList[i] = myMainTransportation[i]+" ";
+  }
+
+  return newList;
+}
+
+//will print out my different modes of transportation
+document.writeln("These are my main form of tansportation: " + addSpace(myMainTransportation) + "<br>");
+//print out my information in its format
+document.writeln("My main ride: <pre>",
+  JSON.stringify(wipInfo, null, '\t'), "</pre>");
+
+//if I own it or not
+ownIt = true;
+
+//will print one or another based on the boolean
+if(ownIt) {
+  document.writeln("I do own it!");
+} else {
+  document.writeln("I don't own it!");
+}
